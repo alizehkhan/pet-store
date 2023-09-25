9e520e3f2681ff1d7b97a4d7cb5a2591bf990026
Alizeh Khan <alizehedinburgh@gmail.com>
2023-09-25 17:50:03 +0200

    feat: vite + eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import', 'unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# md-alizeh_khan
-Matchday Repo for Alizeh Khan
+# Pet Catalogue
+
+An MVP of a pet catalogue with the following features:
+
+- A filterable list of pets
+- A detailed view of each pet
+- The possibility to create a pet
+
+Using this [API](https://editor.swagger.io/?url=https://raw.githubusercontent.com/swagger-api/swagger-petstore/master/src/main/resources/openapi.yaml)
+
+## Tech stack
+
+- React + TypeScript + Vite
+- Tailwind
+- Vitest
+- ESLint
